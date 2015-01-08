b621279b2d9b651f22364101c2a787b5f493b78a
Cauli Tomaz <caulitomaz@gmail.com>
2015-01-08 01:16:56 -0200

    Added database.js and email.js to heroku

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'mysql.cauli.kinghost.net',
+        'user': 'cauli02',
+        'password': 'pfft6vol'
+    },
+	'database': 'cauli02',
+    'users_table': 'users'
+};
---
+++ config/email.js
@@ -0,0 +1,15 @@
+// config/email.js
+module.exports = {
+	host: "smtp.cauli.kinghost.net", // hostname
+	secureConnection: false,
+	port: 587, // port for secure SMTP
+	requiresAuth: true,
+	domains: ["cauli.kinghost.net"],
+	tls: {ciphers:'SSLv3',
+         rejectUnauthorized:false
+    },
+	auth: {
+	user: "checkwithmates@cauli.kinghost.net",
+	pass: "LULLABY666"
+	}
+};
